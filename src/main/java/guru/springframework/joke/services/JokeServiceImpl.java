ccfc3e1bcafb3c2fdbdb8a5cd5b62c21bcf9c496
joel-regen <joel@jregen.com>
2020-09-12 20:55:05 -0400

    make ChuckNorrisJokes a configuration bean.

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -1,14 +1,15 @@
 package guru.springframework.joke.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService{
     private final ChuckNorrisQuotes quotes;
 
-    public JokeServiceImpl() {
-        this.quotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes quotes) {
+        this.quotes = quotes;
     }
 
     @Override
